3d383f2cb65315dac7e080aeb7c2ae49667de1a9
fortunatozm <fortunatompongo@hotmail.com>
2022-03-07 17:26:50 -0300

    1. Crie as rotas necessárias para a aplicação - OK

---
+++ src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        Rota /search
+      </div>
+    );
+  }
+}
+
+export default Search;
