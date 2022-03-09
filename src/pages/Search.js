08ec513dd5f55c32060cfc154833439c2c44b5d4
fortunatozm <fortunatompongo@hotmail.com>
2022-03-09 11:06:41 -0300

    5. Crie o formulário para pesquisar artistas - OK

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisable: true,
+      nome: '',
+    };
+    this.hendleChangeH = this.hendleChangeH.bind(this);
+  }
+
+  hendleChangeH(prevName) {
+    this.setState({
+      nome: prevName.target.value,
+    }, () => {
+      const NAME_LENGTH = 2;
+      const { nome } = this.state;
+      if (nome.length >= NAME_LENGTH) {
+        this.setState({
+          isDisable: false,
+        });
+      } else {
+        this.setState({
+          isDisable: true,
+        });
+      }
+    });
+  }
+
   render() {
+    const { isDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input onChange={ this.hendleChangeH } data-testid="search-artist-input" />
+        <button disabled={ isDisable } type="button" data-testid="search-artist-button">
+          Pesquisar
+        </button>
       </div>
     );
   }
